1788ea06ab3444f436c950a0b6104b9567c24bd2
David Inoa <davidinoa@gmail.com>
2018-12-28 16:40:51 -0400

    solution binary-search-algorithm

---
+++ binary-search-algorithm.js
@@ -0,0 +1,21 @@
+function binarySearch(target, nums) {
+  let floorIndex = -1;
+  let ceilingIndex = nums.length;
+
+  while (floorIndex + 1 < ceilingIndex) {
+    const distance = ceilingIndex - floorIndex;
+    const halfDistance = Math.floor(distance / 2);
+    const guessIndex = floorIndex + halfDistance;
+    const guessValue = nums[guessIndex];
+
+    if (guessValue === target) {
+      return true;
+    } else if (guessValue > target) {
+      ceilingIndex = guessIndex;
+    } else {
+      floorIndex = guessIndex;
+    }
+  }
+
+  return false;
+}
