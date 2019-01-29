6c91939c786a81b4bbdbdea16eb1e86b9411efaf
David Inoa <davidinoa@gmail.com>
2019-01-29 16:58:50 -0400

    solution valid-anagram

---
+++ valid-anagram.js
@@ -0,0 +1,25 @@
+// check whether the second string is a valid anagram of the first one
+
+function validAnagram(first, second) {
+  if (first.length !== second.length) {
+    return false;
+  }
+
+  const lookup = {};
+
+  for (let i = 0; i < first.length; i++) {
+    const letter = first[i];
+    lookup[letter] ? (lookup[letter] += 1) : (lookup[letter] = 1);
+  }
+
+  for (let i = 0; i < second.length; i++) {
+    const letter = second[i];
+    if (!lookup[letter]) {
+      return false;
+    } else {
+      lookup[letter] -= 1;
+    }
+  }
+
+  return true;
+}
