6bbd1d22cbe651cd0b4fecd6242b5d25338721cb
David Inoa <davidinoa@gmail.com>
2019-02-09 23:19:13 -0400

    solution bracket-validator

---
+++ bracket-validator.js
@@ -0,0 +1,28 @@
+function isValid(code) {
+  const openersToClosers = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+  };
+  const openers = new Set(['(', '[', '{']);
+  const closers = new Set([')', ']', '}']);
+  const openersStack = [];
+
+  for (let i = 0; i < code.length; i++) {
+    const char = code.charAt(i);
+
+    if (openers.has(char)) {
+      openersStack.push(char);
+    } else if (closers.has(char)) {
+      if (!openersStack.length) {
+        return false;
+      }
+      const lastUnclosedOpener = openersStack.pop();
+      if (openersToClosers[lastUnclosedOpener] !== char) {
+        return false;
+      }
+    }
+  }
+
+  return openersStack.length === 0;
+}
